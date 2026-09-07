7ddd3afde1b26b8df585f771efba8cfc7881c175
4rno-Chang <ckf1086691@gmail.com>
2026-09-07 16:27:21 +0800

    add filter, listener

---
+++ src/main/java/com/bistroops/listener/InitializerListener.java
@@ -0,0 +1,24 @@
+package com.bistroops.listener;
+
+
+import com.bistroops.util.HibernateUtil;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class InitializerListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("context started");
+		HibernateUtil.getSessionFactory();
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("context ended");
+		HibernateUtil.shutdown();
+	}
+
+}
