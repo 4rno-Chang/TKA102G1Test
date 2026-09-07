c89908ac00d46c014ed43409940daae686d50950
li <simpson200023@gmail.com>
2026-09-07 10:04:58 +0800

    VO

--- src/main/java/com/bistroops/orders/model/OrdersDAO.java
+++ src/main/java/com/bistroops/orders/model/OrdersDAO.java
@@ -3,12 +3,14 @@ package com.bistroops.orders.model;
 import java.util.List;
 
 public interface OrdersDAO {
-	public void insert(); 
-	public void update();
-	public void delete();
-	public OrdersVO findByPrimaryKey();
+	
+	public void insert(OrdersVO orders); 
+	public void update(OrdersVO orders);
+	public void delete(Integer ordersNo);
+	public OrdersVO findByPrimaryKey(Integer ordersNo);
 	public List<OrdersVO> getAll();
 	
-	
+//萬用複合查詢(傳入參數型態Map)(回傳 List)
+//  public List<EmpVO> getAll(Map<String, String[]> map);
 
 }
