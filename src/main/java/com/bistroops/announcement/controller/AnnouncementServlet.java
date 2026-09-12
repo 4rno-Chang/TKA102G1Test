1c462aafde0377e355b263ec21d949425a7c6d11
4rno-Chang <66151387+4rno-Chang@users.noreply.github.com>
2026-09-12 22:45:39 +0800

    Announcement-update

--- src/main/java/com/bistroops/announcement/controller/AnnouncementServlet.java
+++ src/main/java/com/bistroops/announcement/controller/AnnouncementServlet.java
@@ -27,7 +27,7 @@ public class AnnouncementServlet extends HttpServlet {
 	// private AnnouncementService annService;
 //	@Override
 //	public void init() throws ServletException {
-//		annService = new AnnouncementServiceImpl();
+//		annService = new AnnouncementServiceImpl();  
 //	}
 
 	@Override
